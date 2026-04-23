c6af0e3db6c258398622f89ab3dd0b94af557326
jabarullah1112 <jabarullahtn@gmail.com>
2026-04-23 22:47:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,53 +1,56 @@
 import streamlit as st
-from snowflake.snowpark import Session
+from snowflake.snowpark.context import get_active_session
 import pandas as pd
+import requests
 
-# 🔹 Snowflake connection
-connection_parameters = st.secrets["snowflake"]
-session = Session.builder.configs(connection_parameters).create()
+session = get_active_session()
 
-# 🔹 Title
 st.title("🍹 Smoothie Order App")
 
-# 🔹 Name input
+# 🔹 Inputs
 name_on_order = st.text_input("Enter your name")
 
-# 🔹 Fruit list (DBல இருந்து fetch)
 fruit_df = session.table("smoothies.public.fruit_options").to_pandas()
-
-# index start from 1
-fruit_df.index = fruit_df.index + 1
 
 st.subheader("Available Fruits")
 st.dataframe(fruit_df)
 
-# 🔹 Multiselect
-fruit_list = fruit_df.iloc[:, 0].tolist()   # first column values
-ingredients_list = st.multiselect("Choose fruits", fruit_list)
+fruit_name_list = fruit_df["FRUIT_NAME"].tolist()
+fruit_map = dict(zip(fruit_df["FRUIT_NAME"], fruit_df["SEARCH_ON"]))
 
-# 🔹 Convert list → string
-ingredients_string = ",".join(ingredients_list)
+ingredients_list = st.multiselect("Choose fruits", fruit_name_list)
 
-# 🔹 Button
+
+
+
+
+
+# Checkbox
+order_filled = st.checkbox("Order Filled")
+
+# Button
 submit_button = st.button("Submit Order")
 
-# 🔹 Insert logic
 if submit_button:
     if name_on_order and ingredients_string:
+
         query = f"""
-        insert into smoothies.public.orders(name_on_order, ingredients)
-        values ('{name_on_order}','{ingredients_string}')
+        insert into smoothies.public.orders
+        (name_on_order, ingredients, order_filled)
+        values (
+            '{name_on_order}',
+            '{ingredients_string}',
+            {str(order_filled).upper()}
+        )
         """
+
         session.sql(query).collect()
+        st.success("Order placed successfully! ✅")
 
-        st.success(f"{name_on_order} order placed successfully! ✅")
+        
+
     else:
-        st.warning("Please enter name and select fruits")
+        st.warning("Enter name and select fruits")
 
-# 🔹 Debug (optional)
-st.write("Query Preview:")
-st.code(f"""
-insert into smoothies.public.orders(name_on_order, ingredients)
-values ('{name_on_order}','{ingredients_string}')
-""")
+
 
