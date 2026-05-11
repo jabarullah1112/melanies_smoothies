8a354a16c619111eb070f1939bc3afb89963f82b
jabarullah1112 <jabarullahtn@gmail.com>
2026-05-11 17:36:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,7 @@
 import streamlit as st
 from snowflake.snowpark import Session
 
-connection_parameters = {
-    "account": st.secrets["account"],
-    "user": st.secrets["user"],
-    "password": st.secrets["password"],
-    "role": st.secrets["role"],
-    "warehouse": st.secrets["warehouse"],
-    "database": st.secrets["database"],
-    "schema": st.secrets["schema"]
-}
+session = get_active_session()
 
 session = Session.builder.configs(
     connection_parameters
